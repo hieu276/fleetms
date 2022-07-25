2f2971b5468c4a0c8761ac7b583bf0584e194ef1
Duc Hieu <hieund276@gmail.com>
2022-07-25 23:34:03 +0700

    searching and sorting

--- src/main/java/com/hieu276/fleetms/parameters/services/CountryService.java
+++ src/main/java/com/hieu276/fleetms/parameters/services/CountryService.java
@@ -3,6 +3,10 @@ package com.hieu276.fleetms.parameters.services;
 import com.hieu276.fleetms.parameters.models.Country;
 import com.hieu276.fleetms.parameters.repositories.CountryRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,13 +14,14 @@ import java.util.List;
 @Service
 public class CountryService {
 
-    @Autowired private CountryRepositories countryRepository;
+    @Autowired
+    private CountryRepositories countryRepository;
 
-    public List<Country> findAll(){
+    public List<Country> findAll() {
         return countryRepository.findAll();
     }
 
-    public  void save(Country country){
+    public void save(Country country) {
         countryRepository.save(country);
     }
 
@@ -24,8 +29,26 @@ public class CountryService {
         countryRepository.deleteById(id);
     }
 
-    public  Country findById(Integer id){
+    public Country findById(Integer id) {
         return countryRepository.findById(id).orElse(null);
     }
 
+    public List<Country> findByKeyword(String keyword) {
+        return countryRepository.findByKeyword(keyword);
+    }
+
+//    public Page<Country> findAllWithSort(String field, String direction, int pageNumber) {
+//        Sort sort = direction.equalsIgnoreCase(Sort.Direction.ASC.name()) ?
+//                Sort.by(field).ascending() : Sort.by(field).descending();
+//
+//        Pageable pageable = PageRequest.of(pageNumber - 1, 5, sort);
+//
+//        return countryRepository.findAll(pageable);
+//    }
+
+    public List<Country> findAllWithSort(String field, String direction) {
+        Sort sort = direction.equalsIgnoreCase(Sort.Direction.ASC.name()) ?
+                Sort.by(field).ascending() : Sort.by(field).descending();
+        return countryRepository.findAll(sort);
+    }
 }
